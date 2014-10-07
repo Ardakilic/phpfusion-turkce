029143243541e13f25e0d404a66375799cb62de0
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2014-10-07 20:17:47 +0300

    character encoding, duh..

--- locale/Turkish/contact.php
+++ locale/Turkish/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * PHP-Fusion v9 Türkçe Dil Dosyasý
- * @author Arda Kýlýçdaðý
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
  * @link http://arda.pw , http://www.phpfusionturkiye.com
 */
 
