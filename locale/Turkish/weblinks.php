c4c28c1cb7b740510c774fe89b433d4e0f87916b
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2014-10-07 23:44:33 +0300

    weblinks.php tercüme ok

--- locale/Turkish/weblinks.php
+++ locale/Turkish/weblinks.php
@@ -1,10 +1,17 @@
 <?php
-$locale['400'] = "Web Links";
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
+$locale['400'] = "Web Linkleri";
 // Download File Information
-$locale['410'] = "New";
-$locale['411'] = "Date Added:";
-$locale['412'] = "Visits:";
+$locale['410'] = "[YENİ]";
+$locale['411'] = "Eklenme Tarihi:";
+$locale['412'] = "Ziyaret Sayısı:";
 // Downloads Notices
-$locale['430'] = "No Web Link Categories defined";
-$locale['431'] = "No Web Links have been added to this Category";
+$locale['430'] = "Henüz bir kategori oluşturulmamış.";
+$locale['431'] = "Bu kategoriye eklenmiş bir link bulunamadı.";
 ?>
