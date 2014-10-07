2480398234d94d7f3a5b9bf97ea3e0e995708b76
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2014-10-07 21:11:55 +0300

    error.php eklendi

--- locale/Turkish/error.php
+++ locale/Turkish/error.php
@@ -1,8 +1,15 @@
 <?php
-$locale['err401'] = "Unauthorized access to this page!";
-$locale['err403'] = "Access forbidden to this page!";
-$locale['err404'] = "This page was not found on this server!";
-$locale['err500'] = "Internal server error!";
-$locale['errunk'] = "Unknown error";
-$locale['errret'] = "Return to main site";
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
+$locale['err401'] = "Bu sayfaya izinsiz erişim!";
+$locale['err403'] = "Bu sayfaya erişim engellendi!";
+$locale['err404'] = "Girmeye çalıştığınız sayfa sunucuda bulunamadı!";
+$locale['err500'] = "Dahili sunucu hatası!";
+$locale['errunk'] = "Bilinmeyen hata";
+$locale['errret'] = "Ana Siteye Dön";
 ?>
