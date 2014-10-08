3a799af49ecef73cb3f551d854a0633a35eaa9db
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2014-10-08 21:11:40 +0300

    comments.php eklendi

--- locale/Turkish/admin/comments.php
+++ locale/Turkish/admin/comments.php
@@ -1,17 +1,24 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Comments titles
-$locale['400'] = "Edit Comment";
-$locale['401'] = "Comments";
+$locale['400'] = "Yorum Düzenle";
+$locale['401'] = "Yorumlar";
 // Comments messages
-$locale['410'] = "Comment updated";
-$locale['411'] = "Comment deleted";
+$locale['410'] = "Yorum güncellendi";
+$locale['411'] = "Yorum silindi";
 // Edit Comment form
-$locale['420'] = "Disable smileys in this comment";
-$locale['421'] = "Save Comment";
+$locale['420'] = "Bu yorumda yüz ifadelerini gizle";
+$locale['421'] = "Yorumu kaydet";
 // Comments
-$locale['430'] = "Edit";
-$locale['431'] = "Delete";
-$locale['432'] = "User IP:";
-$locale['433'] = "Delete this comment?";
-$locale['434'] = "There are no comments for this item.";
+$locale['430'] = "Düzenle";
+$locale['431'] = "Sil";
+$locale['432'] = "Kullanıcı IP:";
+$locale['433'] = "Bu yorumu silmek istediğinizden emin misiniz?";
+$locale['434'] = "Yorum bulunamadı.";
 ?>
