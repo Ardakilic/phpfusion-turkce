f32a38361aaa37f3f4ba8929f37934112ea33ef0
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2014-10-08 20:49:53 +0300

    article cats.php translated

--- locale/Turkish/admin/article-cats.php
+++ locale/Turkish/admin/article-cats.php
@@ -1,35 +1,42 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Category titles
-$locale['400'] = "Add Article Category";
-$locale['401'] = "Edit Article Category";
-$locale['402'] = "Current Article Categories";
+$locale['400'] = "Makale Kategorisi Ekle";
+$locale['401'] = "Makale Kategorisi Düzenle";
+$locale['402'] = "Mevcut Makale Kategorileri";
 // Category messages
-$locale['410'] = "Article Category added";
-$locale['411'] = "Article Category updated";
-$locale['412'] = "Article Category cannot be deleted";
-$locale['413'] = "There are Articles linked to this category";
-$locale['414'] = "Article Category deleted";
+$locale['410'] = "Makale Kategorisi Eklendi";
+$locale['411'] = "Makale Kategorisi Düzenlendi";
+$locale['412'] = "Makale Kategorisi Silinemedi";
+$locale['413'] = "Bu Kategoriye Bağlı Makaleler Bulunmakta";
+$locale['414'] = "Makale Kategorisi Silindi";
 // Category form
-$locale['420'] = "Category Name:";
-$locale['421'] = "Category Description:";
-$locale['422'] = "Category Sorting:";
-$locale['423'] = "Article ID";
-$locale['424'] = "Article Subject";
-$locale['425'] = "Article Date";
-$locale['426'] = "Ascending";
-$locale['427'] = "Descending";
-$locale['428'] = "Category Access:";
-$locale['429'] = "Save Category";
+$locale['420'] = "Kategori İsmi:";
+$locale['421'] = "Kategori Açıklaması:";
+$locale['422'] = "Kategori Sıralaması:";
+$locale['423'] = "Makale IDsi";
+$locale['424'] = "Makale Başlığı";
+$locale['425'] = "Makale Tarihi";
+$locale['426'] = "Artan";
+$locale['427'] = "Azalan";
+$locale['428'] = "Kategori Erişimi:";
+$locale['429'] = "Kategoriyi Kaydet";
 // Current category options
-$locale['440'] = "Category";
-$locale['441'] = "Access";
-$locale['442'] = "Options";
-$locale['443'] = "Edit";
-$locale['444'] = "Delete";
-$locale['445'] = "No Article Categories defined";
+$locale['440'] = "Kategori";
+$locale['441'] = "Erişim";
+$locale['442'] = "Seçenekler";
+$locale['443'] = "Düzenle";
+$locale['444'] = "Sil";
+$locale['445'] = "Tanımlanmış Makale Kategorisi Bulunamadı";
 // Delete Article category
-$locale['450'] = "Delete this article category?";
+$locale['450'] = "Bu Makale Kategorisini Silmek İstediğinizden Emin Misiniz?";
 // Error Messages
-$locale['460'] = "Please enter a category name.";
-$locale['461'] = "This category already exists.";
+$locale['460'] = "Lütfen bir kategori ismi girin.";
+$locale['461'] = "Bu kategori ismi zaten mevcut.";
 ?>
