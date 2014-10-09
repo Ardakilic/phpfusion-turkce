15daa8ed56dc66409b74bf4c0957f467772e2f8e
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 12:22:57 +0300

    members_include.php tercüme

--- locale/Turkish/admin/members_include.php
+++ locale/Turkish/admin/members_include.php
@@ -1,38 +1,45 @@
 <?php
-$locale['susp00'] = "Members";
-$locale['susp01'] = "Banned";
-$locale['susp02'] = "Unactivated";
-$locale['susp03'] = "Suspended";
-$locale['susp04'] = "Security Banned";
-$locale['susp05'] = "Canceled";
-$locale['susp06'] = "Anonymized";
-$locale['susp07'] = "Deactivated";
-$locale['susp08'] = "Inactive";
-$locale['susp10'] = "Reinstate";
-$locale['susp11'] = "Ban";
-$locale['susp12'] = "Unactivate";
-$locale['susp13'] = "Suspend";
-$locale['susp14'] = "Security Ban";
-$locale['susp15'] = "Cancel";
-$locale['susp16'] = "Anonymize";
-$locale['susp17'] = "Deactivate";
-$locale['susp18'] = "Inactivate";
-$locale['susp_sys'] = "System Action";
-$locale['susp100'] = "User Log entries for %s";
-$locale['susp100b'] = "%s log entries for %s";
-$locale['susp101'] = "All User Log entries for user %s";
-$locale['susp102'] = "Previous <strong>%s</strong> history for user %s";
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
+$locale['susp00'] = "Üyeler";
+$locale['susp01'] = "Banlanan";
+$locale['susp02'] = "İnaktif Edilmiş";
+$locale['susp03'] = "Askıya Alınmış";
+$locale['susp04'] = "Güvenlik Banı Uygulanmış";
+$locale['susp05'] = "İptal Edilmiş";
+$locale['susp06'] = "Anonim Hale Dönüştürülmüş";
+$locale['susp07'] = "Deaktif Edilmiş";
+$locale['susp08'] = "İnaktif";
+$locale['susp10'] = "Eski Durumuna Dönüştür";
+$locale['susp11'] = "Banla";
+$locale['susp12'] = "İnaktif Et";
+$locale['susp13'] = "Geçici Banlama";
+$locale['susp14'] = "Güvenlik Banı Uygula";
+$locale['susp15'] = "İptal Et";
+$locale['susp16'] = "Anonim Hale Dönüştür";
+$locale['susp17'] = "Deaktif Et";
+$locale['susp18'] = "İnaktif Et";
+$locale['susp_sys'] = "Sistem işlemi";
+$locale['susp100'] = "%s için üye geçmiş girdileri (log)";
+$locale['susp100b'] = "%s işleminin %s için bulunan geçmiş girdileri";
+$locale['susp101'] = "%s kullanıcı adına sahip üyenin tüm üyelik girdisi geçmişi";
+$locale['susp102'] = "Daha önceki <strong>%s</strong> girdi geçmişi (%s için)";
 $locale['susp103'] = "ID";
-$locale['susp104'] = "Date";
-$locale['susp105'] = "Type / Reason";
-$locale['susp106'] = "Administrator";
-$locale['susp107'] = "No reason given!";
+$locale['susp104'] = "Tarih";
+$locale['susp105'] = "Tür / Sebep";
+$locale['susp106'] = "Yönetici";
+$locale['susp107'] = "Bir sebep belirtilmemiş!";
 $locale['susp108'] = "IP";
-$locale['susp109'] = "System Action";
-$locale['susp110'] = "This user has no recorded actions in the log.";
-$locale['susp111'] = "Activated";
-$locale['susp112'] = "IP: ";
-$locale['susp113'] = "Lifted";
-$locale['susp114'] = "Suspension Log";
-$locale['susp115'] = "Members";
+$locale['susp109'] = "System işlemi";
+$locale['susp110'] = "Bu üyenin üye girdi geçmişinde bir log kaydı bulunamadı.";
+$locale['susp111'] = "Aktif Edilme";
+$locale['susp112'] = "IP: "; 
+$locale['susp113'] = "Kaldırılma";
+$locale['susp114'] = "Geçici Banlama Logu";
+$locale['susp115'] = "Üyeler";
 ?>
