10f87438993a8fb9b4099f8c6195554e152d43af
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 11:55:20 +0300

    forum_ranks.php translated

--- locale/Turkish/admin/forum_ranks.php
+++ locale/Turkish/admin/forum_ranks.php
@@ -1,40 +1,47 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Forum Ranks Titles
-$locale['400'] = "Add Rank";
-$locale['401'] = "Edit Rank";
-$locale['402'] = "Current Ranks";
-$locale['403'] = "Rank System Disabled";
+$locale['400'] = "Rütbe Ekle";
+$locale['401'] = "Rütbe Düzenle";
+$locale['402'] = "Mevcut Rütbeler";
+$locale['403'] = "Rütbe Sistemi inaktif edildi";
 // News messages
-$locale['410'] = "Rank added";
-$locale['411'] = "Rank updated";
-$locale['412'] = "Rank deleted";
-$locale['413'] = "Only one rank can be assigned to admins and mods";
-$locale['414'] = "Please enter a Rank Title";
+$locale['410'] = "Rütbe eklendi";
+$locale['411'] = "Rütbe güncellendi";
+$locale['412'] = "Rütbe silindi";
+$locale['413'] = "Yönetici ve Moderatörlere sadece bir tek rütbe atanabilir";
+$locale['414'] = "Lütfen bir rütbe başlığı girin";
 // Forum Ranks form
-$locale['420'] = "Rank Title:";
-$locale['421'] = "Rank Image:";
-$locale['422'] = "Rank Posts:";
-$locale['423'] = "Apply to:";
-$locale['424'] = "Members";
-$locale['425'] = "Moderators";
-$locale['426'] = "Administrators";
-$locale['427'] = "Super Administrators";
-$locale['428'] = "Save Rank";
-$locale['429'] = "Rank type:";
-$locale['429a'] = "Special";
-$locale['429b'] = "Admin";
-$locale['429c'] = "Post count";
+$locale['420'] = "Rütbe Adı:";
+$locale['421'] = "Rütbe Resmi:";
+$locale['422'] = "Rütbe Alt Mesaj Sınırı:";
+$locale['423'] = "Uygulanacak Grup:";
+$locale['424'] = "Üyeler";
+$locale['425'] = "Moderatörler";
+$locale['426'] = "Yöneticiler";
+$locale['427'] = "Süper Yöneticiler";
+$locale['428'] = "Rütbeyi Kaydet";
+$locale['429'] = "Rütbe Tipi:";
+$locale['429a'] = "Özel";
+$locale['429b'] = "Yönetici";
+$locale['429c'] = "Forum mesajı adedi";
 // Current Forum Ranks
-$locale['430'] = "Title";
-$locale['431'] = "Applies To";
-$locale['432'] = "Image";
-$locale['433'] = "Posts";
-$locale['434'] = "Options";
-$locale['435'] = "Edit";
-$locale['436'] = "Delete";
-$locale['437'] = "No Forum Ranks defined.";
-$locale['438'] = "Type / Count";
+$locale['430'] = "Başlık";
+$locale['431'] = "Uygulanan Seviye";
+$locale['432'] = "Resim";
+$locale['433'] = "Mesaj";
+$locale['434'] = "Seçenekler";
+$locale['435'] = "Düzenle";
+$locale['436'] = "Sil";
+$locale['437'] = "Henüz Forum Rütbesi Tanımlanmamış.";
+$locale['438'] = "Tip / Adet";
 // Forum Ranks Disabled
-$locale['450'] = "Forum ranks are currently disabled.<br />Please go to the %s Admin Panel to enable.";
-$locale['451'] = "Forum Settings";
+$locale['450'] = "Forum Rütbeleri şu an aktif değil.<br />Lütfen %s bölümüne giderek aktif edin.";
+$locale['451'] = "Forum Ayarları";
 ?>
