485bd6250d9ae212fe67c70f9fa48d10e9c6c158
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 13:09:05 +0300

    polls.php tercüme

--- locale/Turkish/admin/polls.php
+++ locale/Turkish/admin/polls.php
@@ -1,30 +1,37 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Polls titles
-$locale['400'] = "Add Poll";
-$locale['401'] = "Edit Poll";
-$locale['402'] = "Current Polls";
-$locale['403'] = "Preview Poll";
+$locale['400'] = "Anket Ekle";
+$locale['401'] = "Anketi Düzenle";
+$locale['402'] = "Mevcut Anketler";
+$locale['403'] = "Anketi Önizle";
 // Polls messages
-$locale['410'] = "Poll added";
-$locale['411'] = "Poll updated";
-$locale['412'] = "Poll deleted";
+$locale['410'] = "Anket eklendi";
+$locale['411'] = "Anket güncellendi";
+$locale['412'] = "Anket silindi";
 // Edit/Delete Polls
-$locale['420'] = "Current Polls";
-$locale['421'] = "Edit";
-$locale['422'] = "Delete";
+$locale['420'] = "Mevcut Anketler";
+$locale['421'] = "Düzenle";
+$locale['422'] = "Sil";
 // Preview Polls
-$locale['430'] = "Submit Vote";
+$locale['430'] = "Oy Ver";
 // Polls Form
-$locale['431'] = "Poll Title:";
-$locale['432'] = "Option ";
-$locale['433'] = " Close this Poll";
-$locale['434'] = "Started: ";
-$locale['435'] = "Ended: ";
-$locale['436'] = "Add Option";
-$locale['437'] = "Preview Poll";
-$locale['438'] = "Save Poll";
-$locale['439'] = "Undefined";
-$locale['439a'] = "Please enter a poll title";
-$locale['439b'] = "You need to enter at least 1 poll option";
+$locale['431'] = "Anket Başlığı:";
+$locale['432'] = "Seçenek ";
+$locale['433'] = " Bu Anketi Kapat";
+$locale['434'] = "Başlangıç: ";
+$locale['435'] = "Bitiş: ";
+$locale['436'] = "Seçenek Ekle";
+$locale['437'] = "Anket Önizleme";
+$locale['438'] = "Anketi Kaydet";
+$locale['439'] = "Tanımlanmamış";
+$locale['439a'] = "Lütfen bir anket başlığı girin";
+$locale['439b'] = "En az bir adet anket seçeneği girmelisiniz";
 
 ?>
