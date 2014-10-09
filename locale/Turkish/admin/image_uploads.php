bab2caec3d9ea3a076ec3267cbe8711435c56a48
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 12:01:06 +0300

    image_uploads.php translate

--- locale/Turkish/admin/image_uploads.php
+++ locale/Turkish/admin/image_uploads.php
@@ -1,28 +1,35 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Delete Image
-$locale['400'] = "Delete Image";
-$locale['401'] = "Image Deleted";
-$locale['402'] = "Return to Images Admin";
-$locale['403'] = "Return to Admin Index";
+$locale['400'] = "Resim Sil";
+$locale['401'] = "Resim Silindi";
+$locale['402'] = "Resim Yönetimine Geri Dön";
+$locale['403'] = "Site Yönetimine Geri Dön";
 // Upload Image
-$locale['420'] = "Upload Image";
-$locale['421'] = "Filename:";
-$locale['422'] = "Main Images";
-$locale['423'] = "Article Images";
-$locale['424'] = "News Images";
-$locale['425'] = "You can only upload images in gif, jpg or png format";
-$locale['426'] = "Image Uploaded";
-$locale['427'] = "News Cats Images";
+$locale['420'] = "Resim Yükle";
+$locale['421'] = "Dosya İsmi:";
+$locale['422'] = "Ana Resimler";
+$locale['423'] = "İnceleme Resimleri";
+$locale['424'] = "Haber Resimleri";
+$locale['425'] = "Sadece gif, jpg ya da png biçimlerinde yükleme yapabilirsiniz.";
+$locale['426'] = "Resim Yüklendi";
+$locale['427'] = "Haber Kategorisi Resimleri";
 // View Image
-$locale['440'] = "View Image";
-$locale['441'] = "Unknown file format, cannot display image.";
-$locale['442'] = "Delete this image";
+$locale['440'] = "Resmi görüntüle";
+$locale['441'] = "Tanımlanamayan resim biçimi, bu resim görüntülenemez.";
+$locale['442'] = "Bu resmi sil";
 // Image Management
-$locale['460'] = "Images";
-$locale['461'] = "View";
-$locale['462'] = "Delete";
-$locale['463'] = "This folder is empty";
-$locale['464'] = "Update image list for TinyMCE editor";
+$locale['460'] = "Resimler";
+$locale['461'] = "Göster";
+$locale['462'] = "Sil";
+$locale['463'] = "Bu klasör boş";
+$locale['464'] = "TinyMCE editorü için resim listesini güncelle";
 // Popup Error/Warning Messages
-$locale['470'] = "Delete this image?";
+$locale['470'] = "Bu resim silinsin mi?";
 ?>
