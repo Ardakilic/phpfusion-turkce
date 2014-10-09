f0943fbd936f47e41fa84562656ffe2a7bca0d1f
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 12:28:49 +0300

    news-cats.php tercüme

--- locale/Turkish/admin/news-cats.php
+++ locale/Turkish/admin/news-cats.php
@@ -1,25 +1,32 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // News Categories titles
-$locale['400'] = "Edit News Category";
-$locale['401'] = "Add News Category";
-$locale['402'] = "Current News Categories";
+$locale['400'] = "Haber Kategorisini düzenle";
+$locale['401'] = "Haber Kategorisi ekle";
+$locale['402'] = "Mevcut Haber Kategorileri";
 // News Categories messages
-$locale['420'] = "News Category added";
-$locale['421'] = "News Category updated";
-$locale['422'] = "News Category cannot be deleted";
-$locale['423'] = "There are news items linked to this category";
-$locale['424'] = "News Category deleted";
+$locale['420'] = "Haber Kategorisi eklendi";
+$locale['421'] = "Haber Kategorisi güncellendi";
+$locale['422'] = "Haber Kategorisi silinemedi";
+$locale['423'] = "Bu kategoriye bağlı haberler mevcut";
+$locale['424'] = "Haber Kategorisi silindi";
 // News Categories form
-$locale['430'] = "Category Name:";
-$locale['431'] = "Category Image:";
-$locale['432'] = "Save Category";
-$locale['433'] = "Edit";
-$locale['434'] = "Delete";
-$locale['435'] = "No News Categories defined";
-$locale['436'] = "Click Here to upload category images";
+$locale['430'] = "Kategori İsmi:";
+$locale['431'] = "Kategori Resmi:";
+$locale['432'] = "Kategoriyi Kaydet";
+$locale['433'] = "Düzenle";
+$locale['434'] = "Sil";
+$locale['435'] = "Haber Kategorisi bulunamadı";
+$locale['436'] = "Haber Kategorisi Resmi yüklemek için tıklayın";
 // Popup Error/Warning Messages
-$locale['450'] = "Delete this news category?";
+$locale['450'] = "Bu Haber kategorisini silmek istediğinizden emin misiniz?";
 // Error Messages
-$locale['460'] = "Please enter a category name.";
-$locale['461'] = "This category already exists.";
+$locale['460'] = "Lütfen bir kategori adı belirtin.";
+$locale['461'] = "Bu kategori adı zaten mevcut.";
 ?>
