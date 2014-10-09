35b5f8b3acca01ed9194e280c2c7d1169a4b1e83
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 11:23:28 +0300

    custom_pages.php eklendi

--- locale/Turkish/admin/custom_pages.php
+++ locale/Turkish/admin/custom_pages.php
@@ -1,37 +1,44 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Custom Pages titles
-$locale['400'] = "Add Custom Page";
-$locale['401'] = "Edit Custom Page";
-$locale['402'] = "Current Custom Pages";
+$locale['400'] = "Özel Sayfa Ekle";
+$locale['401'] = "Özel Sayfa Düzenle";
+$locale['402'] = "Mevcut Özel Sayfalar";
 // Custom Pages messages
-$locale['410'] = "Custom Page added";
-$locale['411'] = "Custom Page updated";
-$locale['412'] = "The url to this page is: ";
-$locale['413'] = "Custom Page deleted";
+$locale['410'] = "Özel sayfa eklendi";
+$locale['411'] = "Özel sayfa güncellendi";
+$locale['412'] = "Bu sayfanın adresi: ";
+$locale['413'] = "Özel sayfa silindi";
 // Custom Page form
-$locale['420'] = "Edit";
-$locale['421'] = "Delete";
-$locale['422'] = "Page Title:";
-$locale['423'] = "Access: ";
-$locale['424'] = "Page Content:";
-$locale['425'] = "Admin Password:";
-$locale['426'] = "Add link to navigation menu";
-$locale['427'] = "Enable Comments";
-$locale['428'] = "Enable Ratings";
-$locale['429'] = "Preview Page";
-$locale['430'] = "Save Page";
+$locale['420'] = "Düzenle";
+$locale['421'] = "Sil";
+$locale['422'] = "Sayfa Başlığı:";
+$locale['423'] = "Erişim: ";
+$locale['424'] = "Sayfa İçeriği:";
+$locale['425'] = "Yönetici Parolası:";
+$locale['426'] = "Ana Menüye Link Ekle";
+$locale['427'] = "Yorumlara İzin Ver";
+$locale['428'] = "Oy Kullanılmasına İzin Ver";
+$locale['429'] = "Sayfayı Önizle";
+$locale['430'] = "Sayfayı Kaydet";
 $locale['431'] = "Pagebreak";
 // Popup errors / warning messages
-$locale['450'] = "Delete this custom page?";
-$locale['451'] = "Please specify a page title";
-$locale['452'] = "Please specify your password";
-$locale['453'] = "Admin password doesn't match";
-$locale['454'] = "%s are disabled for all items.<br />This setting will only take effect when they are enabled again.";
-$locale['455'] = "Comments";
-$locale['456'] = "Ratings";
-$locale['457'] = $locale['455']." and ".$locale['456'];
+$locale['450'] = "Bu Özel Sayfa Silinsin Mi?";
+$locale['451'] = "Lütfen sayfa başlığı belirtin";
+$locale['452'] = "Lütfen admin parolanızı belirtin";
+$locale['453'] = "Yönetici parolaları uyuşmuyor";
+$locale['454'] = "%s tüm sayfalar için kapatılmıştır.<br />Ayarı açılana kadar bu durum devam edecektir.";
+$locale['455'] = "Yorumlar";
+$locale['456'] = "Oylama";
+$locale['457'] = $locale['455']." ve ".$locale['456'];
 // TinyMCE editor
 $locale['460'] = "TinyMCE:";
-$locale['461'] = "Enable";
-$locale['462'] = "Disable";
+$locale['461'] = "Aktifleştir";
+$locale['462'] = "Pasifleştir";
 ?>
