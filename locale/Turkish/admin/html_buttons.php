9a051d88288f3fe1a96f6466c20152e99a1cc531
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 11:58:44 +0300

    html_buttons.php tercüme

--- locale/Turkish/admin/html_buttons.php
+++ locale/Turkish/admin/html_buttons.php
@@ -1,21 +1,28 @@
 <?php
-$locale['html400'] = "- Select Color -";
-$locale['html401'] = "- Select Image -";
-$locale['html402'] = "Maroon";
-$locale['html403'] = "Red";
-$locale['html404'] = "Orange";
-$locale['html405'] = "Brown";
-$locale['html406'] = "Yellow";
-$locale['html407'] = "Green";
-$locale['html408'] = "Lime";
-$locale['html409'] = "Olive";
-$locale['html410'] = "Cyan";
-$locale['html411'] = "Blue";
-$locale['html412'] = "Navy Blue";
-$locale['html413'] = "Purple";
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
+$locale['html400'] = "- Renk Seçin -";
+$locale['html401'] = "- Resim Seçin -";
+$locale['html402'] = "Kestane Rengi";
+$locale['html403'] = "Kırmızı";
+$locale['html404'] = "Turuncu";
+$locale['html405'] = "Kahverengi";
+$locale['html406'] = "Sarı";
+$locale['html407'] = "Yeşil";
+$locale['html408'] = "Açık yeşil";
+$locale['html409'] = "Zeytin rengi";
+$locale['html410'] = "Cam Göbeği";
+$locale['html411'] = "Mavi";
+$locale['html412'] = "Lacivert";
+$locale['html413'] = "Mor";
 $locale['html414'] = "Violet";
-$locale['html415'] = "Black";
-$locale['html416'] = "Gray";
-$locale['html417'] = "Silver";
-$locale['html418'] = "White";
+$locale['html415'] = "Siyah";
+$locale['html416'] = "Gri";
+$locale['html417'] = "Gümüş Rengi";
+$locale['html418'] = "Beyaz";
 ?>
