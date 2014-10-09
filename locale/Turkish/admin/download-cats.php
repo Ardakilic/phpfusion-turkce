11fce612a41dde54356aa3bbd1f06b80e8199cd6
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-09 11:41:48 +0300

    download-cats.php tercüme edildi

--- locale/Turkish/admin/download-cats.php
+++ locale/Turkish/admin/download-cats.php
@@ -1,35 +1,42 @@
 <?php
+
+/**
+ * PHP-Fusion v9 Türkçe Dil Dosyası
+ * @author Arda Kılıçdağı
+ * @link http://arda.pw , http://www.phpfusionturkiye.com
+*/
+
 // Download Category titles
-$locale['400'] = "Edit Download Category";
-$locale['401'] = "Add Download Category";
-$locale['402'] = "Current Download Categories";
+$locale['400'] = "Dosya Kategorisini Düzenle";
+$locale['401'] = "Dosya Kategorisi Ekle";
+$locale['402'] = "Mevcut Dosya Kategorileri";
 // Download Category messages
-$locale['410'] = "Download Category added";
-$locale['411'] = "Download Category updated";
-$locale['412'] = "Download Category cannot be deleted";
-$locale['413'] = "There are downloads linked to this category";
-$locale['414'] = "Download Category deleted";
+$locale['410'] = "Dosya kategorisi eklendi";
+$locale['411'] = "Dosya kategorisi güncellendi";
+$locale['412'] = "Dosya kategorisi silinemedi";
+$locale['413'] = "Mevcut kategoride dosyalar bulunmakta";
+$locale['414'] = "Dosya kategorisi silindi";
 // Download Category form
-$locale['420'] = "Category Name:";
-$locale['421'] = "Category Description:";
-$locale['422'] = "Category Sorting:";
-$locale['423'] = "Download ID";
-$locale['424'] = "Download Title";
-$locale['425'] = "Download Date";
-$locale['426'] = "Ascending";
-$locale['427'] = "Descending";
-$locale['428'] = "Category Access:";
-$locale['429'] = "Save Category";
+$locale['420'] = "Kategori İsmi:";
+$locale['421'] = "Kategori Açıklaması:";
+$locale['422'] = "Kategori Sıralaması:";
+$locale['423'] = "Dosya ID";
+$locale['424'] = "Dosya Adı";
+$locale['425'] = "Dosya Tarihi";
+$locale['426'] = "Artan";
+$locale['427'] = "Azalan";
+$locale['428'] = "Kategori Erişimi:";
+$locale['429'] = "Kategoriyi Kaydet";
 // Current Download Categories
-$locale['440'] = "Category";
-$locale['441'] = "Access";
-$locale['442'] = "Options";
-$locale['443'] = "Edit";
-$locale['444'] = "Delete";
-$locale['445'] = "There are no Download Categories defined";
+$locale['440'] = "Kategori";
+$locale['441'] = "Erişim";
+$locale['442'] = "Seçenekler";
+$locale['443'] = "Düzenle";
+$locale['444'] = "Sil";
+$locale['445'] = "Dosya Kategorisi Bulunamadı";
 // Delete Download category
-$locale['450'] = "Delete this download category?";
+$locale['450'] = "Bu Dosya Kategorisini Silmek İstiyor Musunuz?";
 // Error Messages
-$locale['460'] = "Please enter a category name.";
-$locale['461'] = "This category already exists.";
+$locale['460'] = "Lütfen bir kategori ismi girin.";
+$locale['461'] = "Bu kategori zaten mevcut.";
 ?>
